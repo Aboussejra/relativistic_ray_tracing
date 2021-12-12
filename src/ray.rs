ff2d564be3ecaa875c1242e803d5d189f8b07e85
Amir Boussejra <amir@boussejra.com>
2021-12-12 14:18:49 +0100

    [Ray] Second derivate calculations needed for
    different positions and position derivates, for integration in RK4, it
    will be needed on four different positions/position_derivates

--- src/ray.rs
+++ src/ray.rs
@@ -1,5 +1,4 @@
 use crate::space::Space;
-
 #[derive(Debug, Default, Clone, Copy, PartialEq, PartialOrd)]
 pub struct Ray {
     pub position: [f64; 4],
@@ -11,21 +10,6 @@ impl Ray {
         Default::default()
     }
 
-    fn second_derivative(&self, mut space: Space) -> [f64; 4] {
-        space.update_christoffel(&self.position);
-        let mut second_derivative = [0.; 4];
-        for i in 0..4 {
-            for j in 0..4 {
-                for k in 0..4 {
-                    second_derivative[i] -= space.christoffel[[i, j, k]]
-                        * self.position_derivative[j]
-                        * self.position_derivative[k];
-                }
-            }
-        }
-        second_derivative
-    }
-
     fn next_step(&mut self, d_lambda: f64) {
         todo!();
     }
@@ -33,4 +17,22 @@ impl Ray {
     fn trace(&mut self, number_steps: i32) {
         todo!();
     }
+}
+
+fn second_derivative(
+    position: &[f64; 4],
+    position_derivative: &[f64; 4],
+    mut space: Space,
+) -> [f64; 4] {
+    space.update_christoffel(position);
+    let mut second_derivative = [0.; 4];
+    for i in 0..4 {
+        for j in 0..4 {
+            for k in 0..4 {
+                second_derivative[i] -=
+                    space.christoffel[[i, j, k]] * position_derivative[j] * position_derivative[k];
+            }
+        }
+    }
+    second_derivative
 }
